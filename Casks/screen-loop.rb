1987aba09e925f5fc8d00d1ff3935b7c391e1df5
Dmitriy Smotrov <dsxack@gmail.com>
2026-05-16 05:50:13 +0200

    Remove redundant screen-loop cask verification

--- Casks/screen-loop.rb
+++ Casks/screen-loop.rb
@@ -5,8 +5,7 @@ cask "screen-loop" do
   version "0.8.0"
   sha256 "c15becf9a55cad3647e88411f0a505ef231202226f88803dd8c587125ec90a48"
 
-  url "https://github.com/dsxack/screen-loop/releases/download/v#{version}/ScreenLoop-0.8.0-macos-universal.zip",
-      verified: "github.com/dsxack/screen-loop/"
+  url "https://github.com/dsxack/screen-loop/releases/download/v#{version}/ScreenLoop-0.8.0-macos-universal.zip"
   name "Screen Loop"
   desc "Menu bar screen recorder with a rolling buffer for saving recent history"
   homepage "https://github.com/dsxack/screen-loop"
